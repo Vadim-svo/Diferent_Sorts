50d3c3ad8d36eaf768f40f4e8d6b757857339eae
Дягилев Вадим Георгиевич <v.dyagilev@main.nsu.ru>
2024-11-23 14:20:25 +0700

    вроде понял

--- sorts.py
+++ sorts.py
@@ -35,23 +35,23 @@ def insertion_sort(a):
 def quick_sort(arr, low, high):
     if low < high:
         pivot_index = partition(arr, low, high)
-        quick_sort(arr, low, pivot_index - 1)
-        quick_sort(arr, pivot_index + 1, high)
+        quick_sort(arr, low, pivot_index - 1) #сортируется левая часть
+        quick_sort(arr, pivot_index + 1, high) #сортируется правая часть
 
 
 def partition(arr, low, high):
     pivot = arr[low]  # Опорный элемент — первый элемент
     i = low + 1
     for j in range(low + 1, high + 1):
-        if arr[j] <= pivot:
-            arr[i], arr[j] = arr[j], arr[i]
-            i += 1
-    arr[low], arr[i - 1] = arr[i - 1], arr[low]
+        if arr[j] <= pivot:  #если элемент меньше опорного
+            arr[i], arr[j] = arr[j], arr[i]  #элементы меняются местами
+            i += 1  #счётчик
+    arr[low], arr[i - 1] = arr[i - 1], arr[low] #меняем опорный элемент на последний
     return i - 1  # Возвращаем индекс опорного элемента
 
 
 def bose_nelson(data):
-    m = 1
+    m = 1 #размер груп, которые будут сортироваться
     while m < len(data):
         j = 0
         while j + m < len(data):
@@ -62,13 +62,13 @@ def bose_nelson(data):
 
 
 def bose_nelson_merge(data, j, r, m):
-    if j + r < len(data):
+    if j + r < len(data): #если индексы находятся в пределах массива
         if m == 1:
-            if data[j] > data[j + r]:
-                data[j], data[j + r] = data[j + r], data[j]
+            if data[j] > data[j + r]: #сравниваем 2 элемента
+                data[j], data[j + r] = data[j + r], data[j]  #меняем их местами
         else:
-            m = m // 2
-            bose_nelson_merge(data, j, r, m)
+            m = m // 2 #уменьшаем размер группы вдвое
+            bose_nelson_merge(data, j, r, m)   #слияние мелких групп
             if j + r + m < len(data):
                 bose_nelson_merge(data, j + m, r, m)
             bose_nelson_merge(data, j + m, r - m, m)
