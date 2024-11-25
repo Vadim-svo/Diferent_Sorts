248186c0cc7b2d8cad4d763d13d58f2c76580e59
Дягилев Вадим Георгиевич <v.dyagilev@main.nsu.ru>
2024-11-25 08:56:03 +0700

    доделал редме файл

--- sorts_time.py
+++ sorts_time.py
@@ -3,7 +3,7 @@ import time  # Модуль для замера времени
 import sorts  # Импортируем наш модуль с сортировками
 
 # Размер массива
-N = 1000  # Определите здесь желаемый размер массива
+N = 1000000  # Определите здесь желаемый размер массива
 
 # Генерация массива случайных чисел
 original_array = [random.randint(1, 100000) for i in range(N)]
@@ -19,11 +19,11 @@ def measure_sort_time(sort_function, data):
 sort_results = {}
 
 # Проверка времени работы различных сортировок
-sort_results['Bubble Sort'] = measure_sort_time(sorts.bubble_sort, original_array.copy())
-sort_results['Selection Sort'] = measure_sort_time(sorts.selection_sort, original_array.copy())
-sort_results['Insertion Sort'] = measure_sort_time(sorts.insertion_sort, original_array.copy())
+#sort_results['Bubble Sort'] = measure_sort_time(sorts.bubble_sort, original_array.copy())
+#sort_results['Selection Sort'] = measure_sort_time(sorts.selection_sort, original_array.copy())
+#sort_results['Insertion Sort'] = measure_sort_time(sorts.insertion_sort, original_array.copy())
 sort_results['Quick Sort'] = measure_sort_time(lambda x: sorts.quick_sort(x, 0, len(x) - 1), original_array.copy())
-sort_results['Bose-Nelson Sort'] = measure_sort_time(sorts.bose_nelson, original_array.copy())
+#sort_results['Bose-Nelson Sort'] = measure_sort_time(sorts.bose_nelson, original_array.copy())
 
 # Вывод результатов
 for sort_name, time_taken in sort_results.items():
